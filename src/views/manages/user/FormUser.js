cbc8035b7e6c458a8fedf33ef84c13ead665aaa4
nguyenletrung <nguyenletrunghc@gmail.com>
2023-03-24 21:01:24 +0700

    update

---
+++ src/views/manages/user/FormUser.js
@@ -0,0 +1,237 @@
+import React, { useEffect } from "react";
+import { Grid } from "@mui/material";
+import { useForm, Form } from "ui-component/useForm";
+import Controls from "ui-component/controls/Controls";
+import { useState } from "react";
+import { getListDistricByProvinceId, getListProvince, getListWardbyDistrictId } from "services/AccountService";
+
+
+const FormUser = (props) => {
+
+    const initialFValues = {
+        username: '',
+        code: '',
+        gender: '',
+        date: '',
+        email: '',
+        phone: '',
+        address: '',
+        province: '',
+        district: '',
+        ward: '',
+        image: ''
+    }
+
+    const genders = [
+        {
+            id: 0,
+            title: 'Male',
+        },
+        {
+            id: 1,
+            title: 'Female'
+        }
+    ]
+
+    const { addOrEdit, recordForEdit } = props
+
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('username' in fieldValues)
+            temp.username = fieldValues.username ? "" : "This fields is required."
+        if ('phone' in fieldValues)
+            temp.phone = fieldValues.phone ? "" : "This fields is required."
+        if ('address' in fieldValues)
+            temp.address = fieldValues.address ? "" : "This fields is required."
+            
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const [lstProvince, setLstProvince] = useState([])
+    const [lstDistrict, setLstDistrict] = useState([])
+    const [lstWards, setLstWards] = useState([])
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFValues, true, validate);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()) {
+            addOrEdit(values, resetForm);
+        }
+    }
+
+    useEffect(() => {
+        if (recordForEdit != null)
+            setValues({
+                ...recordForEdit
+            })
+        getListProvinces();
+    }, [recordForEdit])
+
+    const getListProvinces = () => {
+        getListProvince()
+        .then(response => {
+            let list = [];
+            response.forEach(item => {
+                let customItem = {};
+                customItem = {...item, id: item.id , title: item.name};
+                list = [...list, customItem];
+            });
+            setLstProvince(list);
+        }).catch(error => {
+            console.log(error)
+        });
+
+    }
+
+    const handleChangeProvince = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setValues({
+            ...values,
+            province: value
+        })
+        // Lấy ds huyện theo id tỉnh
+        getListDistricByProvinceId(value)
+        .then(response => {
+            let list = [];
+            response.forEach(item => {
+                let customItem = {};
+                customItem = {...item, id: item.id , title: item.name};
+                list = [...list, customItem];
+            });
+            setLstDistrict(list);
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+    
+    const handleChangeDistrict = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setValues({
+            ...values,
+            district: value
+        })
+
+        // Lấy danh sách phường/xã theo id quận/huyện
+        getListWardbyDistrictId(value)
+        .then(response => {
+            let list = [];
+            response.forEach(item => {
+                let customItem = {};
+                customItem = {...item, id: item.id, title: item.name}
+                list = [...list, customItem];
+            })
+            setLstWards(list);
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Grid container style={{ width: '700px' }}>
+                <Grid item xs={6} style={{ marginBottom: '15px' }}>
+                    <Controls.Input
+                        name="username"
+                        label="EmployeeName"
+                        value={values.username}
+                        onChange={handleInputChange}
+                        error={errors.username}
+                    />
+                    <Controls.Input
+                        name="code"
+                        label="EmployeeCode"
+                        value={values.code}
+                        onChange={handleInputChange}
+                        error={errors.code}
+                    />
+                    <Controls.RadioGroup
+                        name="gender"
+                        label="Gender"
+                        items={genders}
+                        value={values.gender}
+                        onChange={handleInputChange}
+                        error={errors.gender}
+                    />
+                    <Controls.BasicDatePicker
+                        name="date"
+                        label="Date"
+                        value={values.date}
+                        onChange={handleInputChange}
+                        error={errors.date}
+                    />
+                    <Controls.Input
+                        name="email"
+                        label="Email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                        error={errors.email}
+                    />  
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: '15px' }}>
+                    <Controls.Input
+                        name="phone"
+                        label="Phone"
+                        value={values.phone}
+                        onChange={handleInputChange}
+                        error={errors.phone}
+                    />
+                    <Controls.Select 
+                        options={lstProvince}
+                        value={values.province}
+                        onChange={handleChangeProvince}
+                        name="province"
+                        label="Province"
+                    />
+                    <Controls.Select 
+                        options={lstDistrict}
+                        value={values.district}
+                        onChange={handleChangeDistrict}
+                        name="district"
+                        label="District"
+                    />
+                    <Controls.Select
+                        options={lstWards}
+                        value={values.wards}
+                        onChange={handleInputChange}
+                        name="wards"
+                        label="Ward"
+                    />
+                    <Controls.Input
+                        name="address"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                        error={errors.address}
+                    />
+                </Grid>
+                <Grid item xs={12} style={{ textAlign: 'right' }}>
+                    <div>
+                        <Controls.Button
+                            type="submit"
+                            text="Save"
+                        />
+                    </div>
+                </Grid>
+            </Grid>
+        </Form>
+    )
+}
+
+export default FormUser;
---
+++ src/views/manages/user/ListUser.js
@@ -0,0 +1,200 @@
+// material-ui
+import { Grid, TableBody, TableRow, TableCell, InputAdornment, Toolbar } from '@mui/material';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+import { gridSpacing } from 'store/constant';
+import { useState } from 'react';
+
+import Popup from 'ui-component/Popup';
+import useTable from 'ui-component/useTable';
+import Controls from 'ui-component/controls/Controls';
+import { makeStyles } from '@mui/styles';
+// ===============================|| Dialog ||================================= //
+import { IconEdit, IconTrash, IconSearch } from '@tabler/icons';
+import AddIcon from '@mui/icons-material/Add';
+import { useEffect } from 'react';
+import { showNotification } from 'services/NotificationService';
+import { getListUser, createUser, deleteUser, updateUser } from 'services/AccountService';
+import FormUser from './FormUser';
+const useStyles = makeStyles(theme => ({
+    pageContent: {
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    },
+    searchInput: {
+        width: '75%'
+    },
+    newButton: {
+        position: 'absolute',
+        right: '10px'
+    }
+}));
+
+const ManageUser = () => {
+
+    const classes = useStyles();
+
+    const headCells = [
+        { id: 'id', label: 'STT' },
+        { id: 'username', label: 'NameEmployee' },
+        { id: 'code', label: 'EmployeeCode'},
+        { id: 'email', label: 'Email'},
+        { id: 'phone', label: 'Phone'},
+        { id: 'gender', label: 'Gender'},
+        { id: 'date', label: 'Date'},
+        { id: 'address', label: 'Address'},
+        { id: 'actions', label: 'Actions', disableSorting: true }
+    ]
+
+    const [open, setOpen] = useState(false);
+    const [recordForEdit, setRecordForEdit] = useState(null);
+    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } });
+    const [records, setRecords] = useState([])
+
+    const addOrEdit = (user, resetForm) => {
+        if (user.id) {
+            updateUser(user).then(response => {
+                if (response !== null) {
+                    showNotification("Update User Success", 'success');
+                    getData();
+                }
+            }).catch(error => {
+                showNotification("Update User Fail", 'danger');
+            })
+        } else {
+            createUser(user)
+            .then(response => {
+                showNotification("Create User Success", 'success');
+            });
+        }
+        resetForm()
+        setRecordForEdit(null)
+        setOpen(false)
+    }
+
+    const {
+        TblContainer,
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
+    } = useTable(records, headCells, filterFn);
+
+    const openInPopup = (item) => {
+        console.log(item);
+        setRecordForEdit(item);
+        setOpen(true);
+    }
+
+    const handleSearch = (e) => {
+        let target = e.target;
+        setFilterFn({
+            fn: items => {
+                if (target.value == "") {
+                    console.log(items);
+                    return items;
+                }
+                else{
+                    return items.filter(x => x.username.toLowerCase().includes(target.value.toLowerCase()))
+                }
+            }
+        })
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const getData = () => {
+        getListUser()
+            .then(response => {
+                setRecords(response);
+            }).catch(error => {
+                console.log(error)
+            });
+    };
+
+    const deleteUsers = (item) => {
+        deleteUser(item).then(response => {
+            if (response !== null) {
+                showNotification("Delete User Success", 'success');
+                getData();
+            }
+        }).catch(error => {
+            console.log(error);
+            showNotification("Delete User Fail", 'danger');
+        });
+    }
+
+    return (
+        <>
+            <MainCard title="List Employee">
+                <Toolbar>
+                    <Controls.Input
+                        label="Search"
+                        className={classes.searchInput}
+                        placeholder="Search ...."
+                        InputProps={{
+                            startAdornment: (<InputAdornment position="start">
+                                <IconSearch />
+                            </InputAdornment>)
+                        }}
+                        onChange={handleSearch}
+                    />
+                    <Controls.Button
+                        text="Add New"
+                        variant="contained"
+                        startIcon={<AddIcon />}
+                        className={classes.newButton}
+                        onClick={() => { setOpen(true); setRecordForEdit(null); }}
+                    />
+                </Toolbar>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} style={{overflow: 'auto'}}>
+                        {/* === Table === */}
+                        <TblContainer>
+                            <TblHead />
+                            <TableBody>
+                                {
+                                    recordsAfterPagingAndSorting().map((item, index) =>
+                                    (<TableRow key={item.id}>
+                                        <TableCell>{index + 1}</TableCell>
+                                        <TableCell>{item.username}</TableCell>
+                                        <TableCell>{item.code}</TableCell>
+                                        <TableCell>{item.email}</TableCell>
+                                        <TableCell>{item.phone}</TableCell>
+                                        <TableCell>{item.gender}</TableCell>
+                                        <TableCell>{item.date}</TableCell>
+                                        <TableCell>{item.address}</TableCell>
+                                        <TableCell>
+                                            <Controls.ActionButton
+                                                color="primary"
+                                                onClick={() => { openInPopup(item) }}
+                                            >
+                                                <IconEdit />
+                                            </Controls.ActionButton>
+                                            <Controls.ActionButton
+                                                color="secondary"
+                                                onClick = {() => { deleteUsers(item) }}
+                                            >
+                                                <IconTrash color='red' />
+                                            </Controls.ActionButton>
+                                        </TableCell>
+                                    </TableRow>)
+                                    )
+                                }
+                            </TableBody>
+                        </TblContainer>
+                        <TblPagination />
+                    </Grid>
+                </Grid>
+            </MainCard>
+            <Popup title="Create Employee" openPopup={open} setOpenPopup={setOpen}>
+                <FormUser recordForEdit={recordForEdit}
+                    addOrEdit={addOrEdit} />
+            </Popup>
+        </>
+    );
+};
+
+export default ManageUser;
